35bdfbfc06542355b51b841274847a4f61c4baf2
Harish <70161925+harishjr11@users.noreply.github.com>
2025-01-09 08:47:18 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import Button from './button';
+import Button from './Button';
 import { TiLocationArrow } from 'react-icons/ti';
 import { HiMenu, HiX } from 'react-icons/hi'; // Icons for the hamburger menu
 import { useWindowScroll } from 'react-use';
